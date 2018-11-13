63398df43ccb58984330c970faee5b2c3af50a01
Jacob Wahlgren <jacob@dstsrc.net>
2018-11-13 13:23:14 +0100

    Refactor delete-comment.php

--- delete-comment.php
+++ delete-comment.php
@@ -2,26 +2,33 @@
 require_once 'lib/user.php';
 require_once 'lib/http.php';
 require_once 'lib/db.php';
-$comment_id = (int)$_POST['id'];
-$error = TRUE;
 
-if (isset($current_user) && isset($comment_id)) {
+$comment_id = (int)$_POST['id'];
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST')
+    http_response_code(HTTP_METHOD_NOT_ALLOWED);
+else if (!$comment_id)
+    http_response_code(HTTP_UNPROCESSABLE);
+else if (!$current_user)
+    http_response_code(HTTP_FORBIDDEN);
+else {
     $user_id = $current_user['id'];
+    $error = TRUE;
     if ($db->connected()) {
         $recipe_name = $db->getRecipeNameFromComment($comment_id);
-        if (isset($recipe_name)) {
+        if ($recipe_name) {
             if ($db->deleteComment($user_id, $comment_id)) {
                 $error = FALSE;
             }
         }
     }
-} else {
-    error_log('delete_comment.php without required parameters');
+    if (!$recipe_name)
+        http_response_code(HTTP_INTERNAL_ERROR);
+    else {
+        if ($error)
+            $status = 'delete_failed';
+        else
+            $status = 'deleted';
+        http_redirect("/recipe.php?name=$recipe_name&comment=$status#comments");
+    }
 }
-
-if (!$recipe_name)
-    http_response_code(HTTP_INTERNAL_ERROR);
-else if ($error)
-    http_redirect("/recipe.php?name=$recipe_name&comment=delete_failed#comments");
-else
-    http_redirect("/recipe.php?name=$recipe_name&comment=deleted#comments");
